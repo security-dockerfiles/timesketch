36166918cca6e0fd2778fa2c83882a99e18196b5
Ilya Glotov <contact@ilyaglotov.com>
2020-02-21 21:57:41 +0300

    Add support for late 2019 release

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 # Use the official Docker Hub Ubuntu latest LTS version base image
-FROM ubuntu:16.04
-
+FROM ubuntu:18.04
 LABEL maintainer "contact@ilyaglotov.com"
+
+ARG COMMIT_TAG=20191220
 
 RUN apt-get update \
   \
@@ -9,9 +10,9 @@ RUN apt-get update \
   && apt-get -y install git \
                         libffi-dev \
                         libfontconfig \
-                        python-dev \
-                        python-pip \
-                        python-psycopg2 \
+                        python3-dev \
+                        python3-pip \
+                        python3-psycopg2 \
                         software-properties-common \
                         \
   # Install Plaso
@@ -22,47 +23,29 @@ RUN apt-get update \
                 python-plaso \
                 \
   # Clone latest timesketch repo
-  && git clone --branch=master \
-              --depth=1 \
-              https://github.com/google/timesketch.git \
-              /usr/local/src/timesketch \
-              \
-  # Install frontend deps: nodejs and yarn
-  && apt-get install -y wget \
-                        apt-transport-https \
-                        \
-  && wget -qO- https://deb.nodesource.com/setup_8.x | bash - \
-  && wget -qO- https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
-  && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
-  && apt-get update \
-  && apt-get install -y nodejs \
-                        yarn \
-                        \
-  && cd /usr/local/src/timesketch \
-  && yarn install \
-  && yarn build \
-  && yarn test \
-  \
+  && git clone --branch=$COMMIT_TAG \
+               --depth=1 \
+               https://github.com/google/timesketch.git \
+               /usr/local/src/timesketch \
+               \
   # Install timesketch
-  && pip install /usr/local/src/timesketch \
+  && pip3 install /usr/local/src/timesketch \
   \
   # Copy the Timesketch configuration file into /etc
-  && cp /usr/local/share/timesketch/timesketch.conf /etc \
-  && chmod 644 /etc/timesketch.conf \
+  && mkdir /etc/timesketch \
+  && cp /usr/local/src/timesketch/data/timesketch.conf /etc/timesketch/ \
+  && chmod 644 /etc/timesketch/timesketch.conf \
   \
   # Clean up
   && apt-get purge -y apt-transport-https \
                       git \
-                      nodejs \
                       wget \
-                      yarn \
                       libfontconfig \
   && apt-get autoremove -y \
-                      \
+                           \
   && rm -rf /usr/local/src/timesketch/.git \
-  && rm -rf /root/.cache \
-  && rm -rf /var/lib/apt/lists/* \
-  && rm -rf /tmp/phantomjs
+            /root/.cache \
+            /var/lib/apt/lists/*
 
 # Copy the entrypoint script into the container
 COPY docker-entrypoint.sh /
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Fill /etc/timesketch.conf with valid data
+# Fill /etc/timesketch/timesketch.conf with valid data
 function prepare {
 	# Sleep while postgres is setting up
 	sleep 10
 
-	# Set SECRET_KEY in /etc/timesketch.conf if it isn't already set
-	if grep -q "SECRET_KEY = u''" /etc/timesketch.conf; then
+	# Set SECRET_KEY in /etc/timesketch/timesketch.conf if it isn't already set
+	if grep -q "SECRET_KEY = '<KEY_GOES_HERE>'" /etc/timesketch/timesketch.conf; then
 		OPENSSL_RAND=$( openssl rand -base64 32 )
 		# Using the pound sign as a delimiter to avoid problems with / being output from openssl
-		sed -i 's#SECRET_KEY = u\x27\x27#SECRET_KEY = u\x27'$OPENSSL_RAND'\x27#' /etc/timesketch.conf
+		sed -i 's#SECRET_KEY = \x27<KEY_GOES_HERE>\x27#SECRET_KEY = \x27'$OPENSSL_RAND'\x27#' /etc/timesketch/timesketch.conf
 	fi
 
 	# Set up the Postgres connection
 	if [ $POSTGRES_USER ] && [ $POSTGRES_PASSWORD ] && [ $POSTGRES_ADDRESS ] && [ $POSTGRES_PORT ]; then
-		sed -i 's#postgresql://<USERNAME>:<PASSWORD>@localhost#postgresql://'$POSTGRES_USER':'$POSTGRES_PASSWORD'@'$POSTGRES_ADDRESS':'$POSTGRES_PORT'#' /etc/timesketch.conf
+		sed -i 's#postgresql://<USERNAME>:<PASSWORD>@localhost#postgresql://'$POSTGRES_USER':'$POSTGRES_PASSWORD'@'$POSTGRES_ADDRESS':'$POSTGRES_PORT'#' /etc/timesketch/timesketch.conf
 	else
 		# Log an error since we need the above-listed environment variables
 		echo "Please pass values for the POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_ADDRESS, and POSTGRES_PORT environment variables"
@@ -24,10 +24,10 @@ function prepare {
 	# Set up the Redis connection for Celery broker
 	if [ $REDIS_ADDRESS ] && [ $REDIS_PORT ]; then
 	        # Turn on the upload feature
-	        sed -i 's#UPLOAD_ENABLED = False#UPLOAD_ENABLED = True#' /etc/timesketch.conf
-	        sed -i 's#UPLOAD_FOLDER = u\x27/tmp\x27#UPLOAD_FOLDER = u\x27/timelines\x27#' /etc/timesketch.conf
+	        sed -i 's#UPLOAD_ENABLED = False#UPLOAD_ENABLED = True#' /etc/timesketch/timesketch.conf
+	        sed -i 's#UPLOAD_FOLDER = \x27/tmp\x27#UPLOAD_FOLDER = \x27/timelines\x27#' /etc/timesketch/timesketch.conf
 	
-	        sed -i 's#redis://127.0.0.1:6379#redis://'$REDIS_ADDRESS':'$REDIS_PORT'#g' /etc/timesketch.conf
+	        sed -i 's#redis://127.0.0.1:6379#redis://'$REDIS_ADDRESS':'$REDIS_PORT'#g' /etc/timesketch/timesketch.conf
 	else
 	        # Log an error since we need the above-listed environment variables
 	        echo "Please pass values for the REDIS_ADDRESS and REDIS_PORT environment variables"
@@ -36,8 +36,8 @@ function prepare {
 
 	# Set up the Elastic connection
 	if [ $ELASTIC_ADDRESS ] && [ $ELASTIC_PORT ]; then
-		sed -i 's#ELASTIC_HOST = u\x27127.0.0.1\x27#ELASTIC_HOST = u\x27'$ELASTIC_ADDRESS'\x27#' /etc/timesketch.conf
-		sed -i 's#ELASTIC_PORT = 9200#ELASTIC_PORT = '$ELASTIC_PORT'#' /etc/timesketch.conf
+		sed -i 's#ELASTIC_HOST = \x27127.0.0.1\x27#ELASTIC_HOST = \x27'$ELASTIC_ADDRESS'\x27#' /etc/timesketch/timesketch.conf
+		sed -i 's#ELASTIC_PORT = 9200#ELASTIC_PORT = '$ELASTIC_PORT'#' /etc/timesketch/timesketch.conf
 	else
 		# Log an error since we need the above-listed environment variables
 		echo "Please pass values for the ELASTIC_ADDRESS and ELASTIC_PORT environment variables"
@@ -45,14 +45,15 @@ function prepare {
 	fi
 
 	# Set up the Neo4j connection
-	if [ $NEO4J_ADDRESS ] && [ $NEO4J_PORT ]; then
-
-        sed -i 's#GRAPH_BACKEND_ENABLED = False#GRAPH_BACKEND_ENABLED = True#' /etc/timesketch.conf
-		sed -i 's#NEO4J_HOST = u\x27127.0.0.1\x27#NEO4J_HOST = u\x27'$NEO4J_ADDRESS'\x27#' /etc/timesketch.conf
-		sed -i 's#NEO4J_PORT = 9200#NEO4J_PORT = '$NEO4J_PORT'#' /etc/timesketch.conf
+	if [ $NEO4J_ADDRESS ] && [ $NEO4J_PORT ] && [ $NEO4J_USERNAME ] && [ $NEO4J_PASSWORD ]; then
+        sed -i 's#GRAPH_BACKEND_ENABLED = False#GRAPH_BACKEND_ENABLED = True#' /etc/timesketch/timesketch.conf
+		sed -i 's#NEO4J_HOST = \x27127.0.0.1\x27#NEO4J_HOST = \x27'$NEO4J_ADDRESS'\x27#' /etc/timesketch/timesketch.conf
+		sed -i 's#NEO4J_PORT = 9200#NEO4J_PORT = '$NEO4J_PORT'#' /etc/timesketch/timesketch.conf
+		sed -i 's#NEO4J_USERNAME = \x27neo4j\x27#NEO4J_USERNAME = '$NEO4J_USERNAME'#' /etc/timesketch/timesketch.conf
+		sed -i 's#NEO4J_PASSWORD = \x27NEO4J_PASSWORD\x27#NEO4J_PASSWORD = '$NEO4J_PASSWORD'#' /etc/timesketch/timesketch.conf
 	else
 		# Log a warning since we may need the above-listed environment variables
-		echo "Please pass values for the NEO4J_ADDRESS and NEO4J_PORT environment variables if you want graph support"
+		echo "Please pass values for the NEO4J_ADDRESS, NEO4J_PORT, NEO4J_USERNAME and NEO4J_PASSWORD environment variables if you want graph support"
 	fi
 }
 
